2e8fe402d1ece65d1237385a2c15cf2f655b8fc2
Mohosin999 <hasanmohosin998@gmail.com>
2023-01-31 21:19:17 +0600

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
-import React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Navbar from "./components/navbar";
-import usePlaylists from "./hooks/usePlaylists";
-import PlaylistCardItem from "./components/playlist-card-item";
-import { Container } from "@mui/system";
-import { Grid } from "@mui/material";
-
 const App = () => {
-  const { playlists, getPlaylistById } = usePlaylists();
-
-  const playlistArray = Object.values(playlists);
-
   return (
     <>
-      <CssBaseline />
-      <Container maxWidth={"lg"} sx={{ marginTop: 16 }}>
-        <Navbar getPlaylistById={getPlaylistById} />
-        {playlistArray.length > 0 && (
-          <Grid container alignItems="stretch">
-            {playlistArray.map((item) => (
-              <Grid item xs={12} md={6} lg={4} mb={2}>
-                <PlaylistCardItem
-                  key={item.id}
-                  playlistThumbnail={item.playlistThumbnail}
-                  playlistTitle={item.playlistTitle}
-                  channelTitle={item.channelTitle}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        )}
-      </Container>
+      {/* <CssBaseline /> */}
+      <h2>Akash</h2>
     </>
   );
 };
