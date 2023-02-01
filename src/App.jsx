0361c53c168aa2df01e3fcc708af2f295f2519d6
Mohosin999 <hasanmohosin998@gmail.com>
2023-02-01 09:36:42 +0600

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "./components/navbar";
 import usePlaylists from "./hooks/usePlaylists";
