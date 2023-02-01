16bf935782860508ed31d0473dc14c35b26ad1bc
Mohosin999 <hasanmohosin998@gmail.com>
2023-02-01 09:22:29 +0600

    url

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,7 @@ const key = import.meta.env.VITE_YOUTUBE_API_KEY;
 
 //
 const getPlaylistItem = async (playlistId, pageToken = "", result = []) => {
-  const URL = `https://www.googleapis.com/youtube/v3/playlistItems?
-key=${key}&part=id, contentDetails, snippet&maxResults=50&
-playlistId=${playlistId}&pageToken=${pageToken}`;
+  const URL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&part=id, contentDetails, snippet&maxResults=50&playlistId=${playlistId}&pageToken=${pageToken}`;
 
   const { data } = await axios.get(URL);
   result = [...result, ...data.items];
@@ -18,10 +16,8 @@ playlistId=${playlistId}&pageToken=${pageToken}`;
   return result;
 };
 
-// get Full Playlist - its call first, then "getPlaylistItem" call inside it
 const getPlaylist = async (playlistId) => {
-  const URL = `https://youtube.googleapis.com/youtube/v3/playlists?
-part=snippet&id=${playlistId}&key=${key}`;
+  const URL = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${playlistId}&key=${key}`;
 
   const { data } = await axios.get(URL);
   let playlistItems = await getPlaylistItem(playlistId);
@@ -32,14 +28,14 @@ part=snippet&id=${playlistId}&key=${key}`;
     thumbnails,
     channelId,
     channelTitle,
-  } = data?.items[0]?.snippet; // this informations for Playlist
+  } = data?.items[0]?.snippet;
 
   playlistItems = playlistItems.map((item) => {
     const {
       title,
       description,
       thumbnails: { medium },
-    } = item.snippet; // this informations for every single video
+    } = item.snippet;
 
     return {
       title,
